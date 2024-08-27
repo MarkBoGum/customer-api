6d95cb6b504c982372c49382760c89865153c6d8
MarkBoGum <markmmuncada.dev@gmail.com>
2024-08-28 03:45:21 +0800

    chore(migrations): create migration file for customers table

---
+++ database/migrations/2024_08_27_183753_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('email')->unique();
+            $table->string('username', 100);
+            $table->string('gender', 10);
+            $table->string('country', 100);
+            $table->string('city', 100);
+            $table->string('phone', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
