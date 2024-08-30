7796c6173dd7b35b15de69fd5a3718059ebfa25f
MarkBoGum <markmmuncada.dev@gmail.com>
2024-08-30 12:44:26 +0800

    test(unit): update unit tests to align with facade removal

--- tests/Unit/CustomerImporterServiceTest.php
+++ tests/Unit/CustomerImporterServiceTest.php
@@ -8,12 +8,15 @@ use App\Contracts\CustomerDataProviderInterface;
 use App\Repositories\CustomerRepository;
 use App\Services\CustomerImporterService;
 use App\Entities\Customer;
-use Illuminate\Support\Facades\Log;
+use App\Transformers\RandomUserTransformer;
+use Psr\Log\LoggerInterface;
 
 class CustomerImporterServiceTest extends TestCase
 {
     protected $dataProviderMock;
     protected $customerRepositoryMock;
+    protected $randomUserTransformerMock;
+    protected $loggerMock;
     protected $importerService;
 
     protected function setUp(): void
@@ -22,10 +25,14 @@ class CustomerImporterServiceTest extends TestCase
 
         $this->dataProviderMock = Mockery::mock(CustomerDataProviderInterface::class);
         $this->customerRepositoryMock = Mockery::mock(CustomerRepository::class);
+        $this->randomUserTransformerMock = Mockery::mock(RandomUserTransformer::class);
+        $this->loggerMock = Mockery::mock(LoggerInterface::class);
 
         $this->importerService = new CustomerImporterService(
             $this->dataProviderMock,
-            $this->customerRepositoryMock
+            $this->customerRepositoryMock,
+            $this->randomUserTransformerMock,
+            $this->loggerMock
         );
     }
 
@@ -57,27 +64,47 @@ class CustomerImporterServiceTest extends TestCase
             ]
         ];
 
+        $transformedData = [
+            'first_name' => 'Warren',
+            'last_name' => 'Byrd',
+            'email' => 'warren.byrd@example.com',
+            'username' => 'tinyfish792',
+            'gender' => 'male',
+            'country' => 'Australia',
+            'city' => 'Perth',
+            'phone' => '05-0360-2392',
+            'password' => md5('tacobell'),
+            'created_at' => new \DateTime(),
+            'updated_at' => new \DateTime(),
+        ];
+
         $this->dataProviderMock
             ->shouldReceive('fetchCustomers')
             ->with(1, 1)
             ->andReturn($mockData);
 
+        $this->randomUserTransformerMock
+            ->shouldReceive('transform')
+            ->with($mockData[0])
+            ->andReturn($transformedData);
+
         $this->customerRepositoryMock
             ->shouldReceive('findByEmail')
+            ->with('warren.byrd@example.com')
             ->andReturn(null); // Simulate no existing customer
 
         $this->customerRepositoryMock
             ->shouldReceive('save')
-            ->andReturnUsing(function ($customer) use ($mockData) {
-                $this->assertEquals($mockData[0]['email'], $customer->getEmail());
-                $this->assertEquals($mockData[0]['name']['first'], $customer->getFirstName());
-                $this->assertEquals($mockData[0]['name']['last'], $customer->getLastName());
-                $this->assertEquals($mockData[0]['login']['username'], $customer->getUsername());
-                $this->assertEquals($mockData[0]['gender'], $customer->getGender());
-                $this->assertEquals($mockData[0]['location']['country'], $customer->getCountry());
-                $this->assertEquals($mockData[0]['location']['city'], $customer->getCity());
-                $this->assertEquals($mockData[0]['phone'], $customer->getPhone());
-                $this->assertEquals(md5($mockData[0]['login']['password']), $customer->getPassword());
+            ->andReturnUsing(function ($customer) use ($transformedData) {
+                $this->assertEquals($transformedData['email'], $customer->getEmail());
+                $this->assertEquals($transformedData['first_name'], $customer->getFirstName());
+                $this->assertEquals($transformedData['last_name'], $customer->getLastName());
+                $this->assertEquals($transformedData['username'], $customer->getUsername());
+                $this->assertEquals($transformedData['gender'], $customer->getGender());
+                $this->assertEquals($transformedData['country'], $customer->getCountry());
+                $this->assertEquals($transformedData['city'], $customer->getCity());
+                $this->assertEquals($transformedData['phone'], $customer->getPhone());
+                $this->assertEquals($transformedData['password'], $customer->getPassword());
             });
 
         $this->customerRepositoryMock
@@ -125,27 +152,47 @@ class CustomerImporterServiceTest extends TestCase
             ]
         ];
 
+        $transformedData = [
+            'first_name' => 'Existing',
+            'last_name' => 'Customer',
+            'email' => 'customer1@example.com',
+            'username' => 'existingusername',
+            'gender' => 'female',
+            'country' => 'Australia',
+            'city' => 'Melbourne',
+            'phone' => '987654322',
+            'password' => md5('securepassword'),
+            'created_at' => new \DateTime(),
+            'updated_at' => new \DateTime(),
+        ];
+
         $this->dataProviderMock
             ->shouldReceive('fetchCustomers')
             ->with(1, 1)
             ->andReturn($mockData);
 
+        $this->randomUserTransformerMock
+            ->shouldReceive('transform')
+            ->with($mockData[0])
+            ->andReturn($transformedData);
+
         $this->customerRepositoryMock
             ->shouldReceive('findByEmail')
+            ->with('customer1@example.com')
             ->andReturn($existingCustomer); // Simulate existing customer
 
         $this->customerRepositoryMock
             ->shouldReceive('save')
-            ->andReturnUsing(function ($customer) use ($mockData) {
-                $this->assertEquals($mockData[0]['email'], $customer->getEmail());
-                $this->assertEquals($mockData[0]['name']['first'], $customer->getFirstName());
-                $this->assertEquals($mockData[0]['name']['last'], $customer->getLastName());
-                $this->assertEquals($mockData[0]['login']['username'], $customer->getUsername());
-                $this->assertEquals($mockData[0]['gender'], $customer->getGender());
-                $this->assertEquals($mockData[0]['location']['country'], $customer->getCountry());
-                $this->assertEquals($mockData[0]['location']['city'], $customer->getCity());
-                $this->assertEquals($mockData[0]['phone'], $customer->getPhone());
-                $this->assertEquals(md5($mockData[0]['login']['password']), $customer->getPassword());
+            ->andReturnUsing(function ($customer) use ($transformedData) {
+                $this->assertEquals($transformedData['email'], $customer->getEmail());
+                $this->assertEquals($transformedData['first_name'], $customer->getFirstName());
+                $this->assertEquals($transformedData['last_name'], $customer->getLastName());
+                $this->assertEquals($transformedData['username'], $customer->getUsername());
+                $this->assertEquals($transformedData['gender'], $customer->getGender());
+                $this->assertEquals($transformedData['country'], $customer->getCountry());
+                $this->assertEquals($transformedData['city'], $customer->getCity());
+                $this->assertEquals($transformedData['phone'], $customer->getPhone());
+                $this->assertEquals($transformedData['password'], $customer->getPassword());
             });
 
         $this->customerRepositoryMock
@@ -163,7 +210,6 @@ class CustomerImporterServiceTest extends TestCase
         $batchSize = 20; // Defined in the service class
         $mockData = [];
 
-        // Generate mock data to match the batch size
         for ($i = 1; $i <= $batchSize; $i++) {
             $mockData[] = [
                 "gender" => "male",
@@ -181,11 +227,33 @@ class CustomerImporterServiceTest extends TestCase
             ];
         }
 
-        // Mock the fetchCustomers method to return the batchSize amount of data
+        $transformedData = [];
+        foreach ($mockData as $i => $data) {
+            $transformedData[] = [
+                'first_name' => $data['name']['first'],
+                'last_name' => $data['name']['last'],
+                'email' => $data['email'],
+                'username' => $data['login']['username'],
+                'gender' => $data['gender'],
+                'country' => $data['location']['country'],
+                'city' => $data['location']['city'],
+                'phone' => $data['phone'],
+                'password' => md5($data['login']['password']),
+                'created_at' => new \DateTime(),
+                'updated_at' => new \DateTime(),
+            ];
+        }
+
         $this->dataProviderMock
             ->shouldReceive('fetchCustomers')
             ->with(1, $batchSize)
             ->andReturn($mockData);
+
+        $this->randomUserTransformerMock
+            ->shouldReceive('transform')
+            ->andReturnUsing(function ($data) use ($transformedData) {
+                return $transformedData[array_search($data['email'], array_column($transformedData, 'email'))];
+            });
 
         $this->customerRepositoryMock
             ->shouldReceive('findByEmail')
@@ -194,12 +262,11 @@ class CustomerImporterServiceTest extends TestCase
         $this->customerRepositoryMock
             ->shouldReceive('save')
             ->times($batchSize)
-            ->andReturnUsing(function ($customer) use ($mockData) {
-                // Extract the index from the email correctly
+            ->andReturnUsing(function ($customer) use ($transformedData) {
                 $index = (int) filter_var($customer->getEmail(), FILTER_SANITIZE_NUMBER_INT) - 1;
-                $this->assertEquals($mockData[$index]['email'], $customer->getEmail());
-                $this->assertEquals($mockData[$index]['name']['first'], $customer->getFirstName());
-                $this->assertEquals($mockData[$index]['name']['last'], $customer->getLastName());
+                $this->assertEquals($transformedData[$index]['email'], $customer->getEmail());
+                $this->assertEquals($transformedData[$index]['first_name'], $customer->getFirstName());
+                $this->assertEquals($transformedData[$index]['last_name'], $customer->getLastName());
             });
 
         $this->customerRepositoryMock
@@ -218,7 +285,8 @@ class CustomerImporterServiceTest extends TestCase
             ->shouldReceive('fetchCustomers')
             ->andThrow(new \Exception('API error'));
 
-        Log::shouldReceive('error')
+        $this->loggerMock
+            ->shouldReceive('error')
             ->once()
             ->with('Failed to import customers: API error');
 
